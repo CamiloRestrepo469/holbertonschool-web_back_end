6d8c6b2c6de2ac81fc96ede1502f7a9b6979373d
CamiloRestrepo469 <rsgermancamilo@gmail.com>
2023-08-31 15:57:32 -0500

    new two file

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -11,19 +11,3 @@ def task_wait_random(max_delay: int) -> asyncio.Task:
     """ Tasks """
     task = asyncio.create_task(wait_random(max_delay))
     return task
-
-# ```python
-# import asyncio
-# wait_random = __import__('0-basic_async_syntax').wait_random
-# ```
-# Se importa el módulo `asyncio`, y luego se importa la función `wait_random` de otro archivo usando el método `__import__()`.
-
-# ```python
-# def task_wait_random(max_delay: int) -> asyncio.Task:
-#     """ Tasks """
-#     task = asyncio.create_task(wait_random(max_delay))
-#     return task
-# ```
-# Se define una función llamada `task_wait_random` que acepta un argumento entero llamado `max_delay`. Dentro de esta función, se crea una tarea asincrónica usando `asyncio.create_task()`. La tarea se crea ejecutando la función `wait_random(max_delay)`, que esperará un tiempo aleatorio asincrónicamente y luego devolverá ese tiempo. La función `task_wait_random` devuelve la tarea creada.
-
-# Esta función `task_wait_random` se encarga de crear una tarea asincrónica basada en la función `wait_random`, que a su vez puede ser ejecutada en paralelo junto con otras tareas usando el módulo `asyncio`.
