acd8d7dcbbbdcbebf9a8a037307bede065cc09f5
CamiloRestrepo469 <rsgermancamilo@gmail.com>
2023-08-31 15:26:12 -0500

    new file test

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -12,3 +12,45 @@ async def wait_random(max_delay: int = 10) -> float:
     random_float = random.uniform(0, max_delay)
     await asyncio.sleep(random_float)
     return random_float
+
+# ```python
+# """ The basics of async  """
+# ```
+# Esta línea es un comentario de triple comilla que sirve como docstring para el módulo. Proporciona una descripción general del propósito del módulo.
+
+# ```python
+# import random
+# import asyncio
+# ```
+# Estas líneas importan los módulos `random` y `asyncio`. `random` se utiliza para generar números aleatorios, y `asyncio` es una librería para programación asíncrona en Python.
+
+# ```python
+# async def wait_random(max_delay: int = 10) -> float:
+# ```
+# Aquí se define una función asíncrona llamada `wait_random` que acepta un argumento entero llamado `max_delay` con un valor predeterminado de 10. La función devuelve un valor de tipo `float`.
+
+# ```python
+#     """ asynchronous coroutine that takes in an integer argument
+#         (max_delay, with a default value of 10) named wait_random that waits
+#         for a random delay between 0 and max_delay (included and float value)
+#         seconds and eventually returns it.  """
+# ```
+# Este es el docstring de la función `wait_random`, que proporciona una descripción de lo que hace la función. Indica que es una "corutina asincrónica" que espera un tiempo de retraso aleatorio entre 0 y `max_delay` segundos, y finalmente devuelve ese valor.
+
+# ```python
+#     random_float = random.uniform(0, max_delay)
+# ```
+# Se genera un número de punto flotante aleatorio utilizando la función `random.uniform()`. Este número estará entre 0 y `max_delay` (incluido).
+
+# ```python
+#     await asyncio.sleep(random_float)
+# ```
+# Aquí se utiliza la función `asyncio.sleep()` para pausar la ejecución de la corutina durante `random_float` segundos. La palabra clave `await` se utiliza para indicar que se está esperando asincrónicamente la finalización de esta operación de pausa.
+
+# ```python
+#     return random_float
+# ```
+# Finalmente, la función devuelve el valor de `random_float` como resultado de la corutina.
+
+# Este fragmento de código se refiere a conceptos de programación asincrónica y corutinas en Python, donde se utilizan las capacidades de `asyncio` para gestionar tareas que pueden ejecutarse de manera asíncrona. La función `wait_random` simula una espera de tiempo variable y asincrónica antes de devolver un valor aleatorio.
+
--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -20,3 +20,49 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
         earliest_result = await delay
         all_delays.append(earliest_result)
     return all_delays
+
+
+# Esta es la docstring del módulo. Proporciona una descripción de alto nivel de lo que hace el código. En este caso, explica que el objetivo es importar la función `wait_random` de otro archivo y escribir una rutina asíncrona llamada `wait_n` que toma dos argumentos enteros: `max_delay` y `n`. La función crea varias corutinas `wait_random` con un retraso máximo dado y devuelve una lista de los retrasos en orden ascendente sin usar `sort()` debido a la concurrencia.
+
+# ```python
+# import asyncio
+# from typing import List
+# wait_random = __import__('0-basic_async_syntax').wait_random
+# ```
+# Se importa el módulo `asyncio` y se importa el tipo de dato `List` del módulo `typing`. Luego se importa la función `wait_random` de otro archivo usando el método `__import__()`.
+
+# ```python
+# async def wait_n(n: int, max_delay: int) -> List[float]:
+# ```
+# Se define una función asíncrona llamada `wait_n` que acepta dos argumentos enteros: `n` y `max_delay`. La función devuelve una lista de valores de punto flotante.
+
+# ```python
+#     """ Let's execute multiple coroutines at the same time with async  """
+# ```
+# Esta es la docstring de la función `wait_n`, que proporciona una breve descripción de lo que hace la función.
+
+# ```python
+#     delays: List[float] = []
+#     all_delays: List[float] = []
+# ```
+# Se declaran dos listas vacías para almacenar los retrasos.
+
+# ```python
+#     for i in range(n):
+#         delays.append(wait_random(max_delay))
+# ```
+# Se utiliza un bucle para crear `n` corutinas `wait_random` con un retraso máximo de `max_delay` y se agrega cada corutina a la lista `delays`.
+
+# ```python
+#     for delay in asyncio.as_completed(delays):
+#         earliest_result = await delay
+#         all_delays.append(earliest_result)
+# ```
+# Aquí, se utiliza `asyncio.as_completed()` para esperar hasta que cada una de las corutinas en `delays` se complete. Cuando una corutina se completa, su resultado se agrega a la lista `all_delays`.
+
+# ```python
+#     return all_delays
+# ```
+# Finalmente, se devuelve la lista `all_delays` que contiene los retrasos en orden ascendente.
+
+# Este código utiliza el módulo `asyncio` para crear y manejar múltiples corutinas asincrónicas en paralelo y recopilar sus resultados en un orden específico. La función `wait_n` toma un número `n` y un retraso máximo `max_delay`, crea `n` corutinas `wait_random`, las ejecuta en paralelo y devuelve una lista ordenada de los resultados.
