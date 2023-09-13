74f37b3da141f733939db38b3ee4025c1d1a3560
CamiloRestrepo469 <rsgermancamilo@gmail.com>
2023-09-13 08:22:14 -0500

    new file

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,21 +1,15 @@
-// En el archivo 0-constants.js
-
-// Función para instanciar variables usando 'const'
 export function taskFirst() {
     const task = 'I prefer const when I can.';
     return task;
-  }
-  
-  // Función para obtener una parte de la cadena
-  export function getLast() {
+}
+
+export function getLast() {
     return ' is okay';
-  }
-  
-  // Función para instanciar variables usando 'let'
-  export function taskNext() {
+}
+
+export function taskNext() {
     let combination = 'But sometimes let';
     combination += getLast();
-  
+
     return combination;
-  }
-  
+}
